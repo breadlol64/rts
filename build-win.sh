61ed0bf40a8a1364fb4e96f5e877b5a2d2e82532
artur <breadlol64@gmail.com>
2026-06-24 15:57:31 +0300

    action

--- build-win.sh
+++ build-win.sh
@@ -9,5 +9,5 @@ fi
 
 odin build .. -target:windows_amd64 -build-mode:obj -out:game.o
 zig 0.16.0 cc -target x86_64-windows-gnu -c ../fltused.c -o fltused.o
-zig 0.16.0 cc -target x86_64-windows-gnu game-*.o fltused.o -L./raylib-5.5_win64_mingw-w64/lib -lraylib -lopengl32 -lgdi32 -lwinmm -lm -lbcrypt -mwindows -o game.exe
+zig 0.16.0 cc -target x86_64-windows-gnu game-*.o fltused.o -L./raylib-5.5_win64_mingw-w64/lib -lraylib -lraygui -lopengl32 -lgdi32 -lwinmm -lm -lbcrypt -mwindows -o game.exe
 cp raylib-5.5_win64_mingw-w64/lib/raylib.dll .
